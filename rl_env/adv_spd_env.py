2762d69482d8d1b7f5d528f0126289d7fe200159
benchoi93 <benchoi93@gmail.com>
2021-08-13 18:18:53 +0900

    disable signal ensure

--- rl_env/adv_spd_env.py
+++ rl_env/adv_spd_env.py
@@ -445,9 +445,9 @@ class AdvSpdEnv(gym.Env):
         applied_action = action
         self.vehicle.actiongap = action
 
-        max_acc = self.calculate_max_acceleration()
-        if max_acc < action:
-            applied_action = max_acc
+        # max_acc = self.calculate_max_acceleration()
+        # if max_acc < action:
+        #     applied_action = max_acc
 
         if self.vehicle.velocity + applied_action * self.dt < 0:
             applied_action = - self.vehicle.velocity / self.dt
